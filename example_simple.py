5ea0cf4435154523b15894f71e40c56259681158
elliottmckee <emckee3779@gmail.com>
2024-08-24 12:53:55 -0700

    file cleanup

--- example_simple.py
+++ example_simple.py
@@ -13,13 +13,13 @@ from src.gen_farfield import gen_farfield
 # input .msh surface mesh
 # can make in gmsh gui, or alternatively, use gmsh python api (see advanced example)
 # must export from GMSH as version 2 ascii!
-SurfMesh = UMesh('resource/rocket_stubby.msh')
+SurfMesh = UMesh('resource/rocket_stubby_surf.msh')
 
 # convert surface mesh from .msh to ugrid
-SurfMesh.write('resource/rocket_stubby.ugrid')
+SurfMesh.write('resource/rocket_stubby_surf.ugrid')
 
 # mesh_tools: extrude BoundaryLayer mesh from surface mesh, convert back to .msh
-BoundLayerMesh = gen_blmesh('resource/rocket_stubby.ugrid', num_bl_layers=10, near_wall_spacing=4.2e-5, bl_growth_rate=1.5)
+BoundLayerMesh = gen_blmesh('resource/rocket_stubby_surf.ugrid', num_bl_layers=10, near_wall_spacing=4.2e-5, bl_growth_rate=1.5)
 BoundLayerMesh.write('resource/rocket_stubby_BLMESH.msh')
 
 # gmsh: generate BoundaryLayer+Farfield mesh, by building around/outward-from the boundary layer mesh

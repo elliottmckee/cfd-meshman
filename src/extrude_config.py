4a788aef3ff5dd06c65b4c08ee84bba87a344759
elliottmckee <emckee3779@gmail.com>
2024-08-24 12:57:54 -0700

    extrude config changes

--- src/extrude_config.py
+++ src/extrude_config.py
@@ -10,7 +10,7 @@ null_space_limit: 1.0
 null_space_relaxation: 0.6
 eigenvector_threshold: 0.5
 smooth_curvature: true
-curvature_factor: 4.0     
+curvature_factor: 5.0     
 symmetry: none 
 symmetry_threshold: 2.0
 unmodified_layers: 3

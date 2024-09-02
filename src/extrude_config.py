b78c6603d10045bb53341a14fb989462573c596a
elliottmckee <emckee3779@gmail.com>
2024-09-02 15:25:10 -0700

    updating default extrude config

--- src/extrude_config.py
+++ src/extrude_config.py
@@ -6,11 +6,11 @@ smooth_normals: true
 smooth_normals_iterations: 25
 smooth_null_space: true
 null_space_iterations: 3
-null_space_limit: 1.0
-null_space_relaxation: 0.6
+null_space_limit: 2.0
+null_space_relaxation: 0.3
 eigenvector_threshold: 0.5
 smooth_curvature: true
-curvature_factor: 5.0     
+curvature_factor: 4.5 
 symmetry: none 
 symmetry_threshold: 2.0
 unmodified_layers: 3
